534e9914ce098dab68142d8e37397fe6aafbcf15
Josh Cain <josh.cain@coxautoinc.com>
2021-08-09 11:25:35 -0400

    added functionality and all favicons

--- next.config.js
+++ next.config.js
@@ -1,3 +1,77 @@
-module.exports = {
-  reactStrictMode: true,
-}
+const withPlugins = require('next-compose-plugins');
+const withImages = require('next-images');
+const webpack = require('webpack');
+const path = require('path');
+
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+	enabled: process.env.ANALYZE === 'true',
+});
+
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' googletagmanager.com *.googletagmanager.com vercel-insights.com *.vercel-insights.com;
+  child-src *.googletagmanager.com *.vercel-insights.com;
+  style-src 'self' 'unsafe-inline' googleapis.com *.googleapis.com;
+  img-src * blob: data:;
+  media-src 'none';
+  connect-src *;
+  font-src 'self' gstatic.com *.gstatic.com;
+`;
+
+const securityHeaders = [
+	{
+		key: 'Content-Security-Policy',
+		value: ContentSecurityPolicy.replace(/\n/g, ''),
+	},
+	{
+		key: 'Referrer-Policy',
+		value: 'origin-when-cross-origin',
+	},
+	{
+		key: 'X-Frame-Options',
+		value: 'DENY',
+	},
+	{
+		key: 'X-Content-Type-Options',
+		value: 'nosniff',
+	},
+	{
+		key: 'X-DNS-Prefetch-Control',
+		value: 'on',
+	},
+	{
+		key: 'Strict-Transport-Security',
+		value: 'max-age=31536000; includeSubDomains; preload',
+	},
+	{
+		key: 'Permissions-Policy',
+		value: 'camera=(), microphone=(), geolocation=()',
+	},
+];
+
+module.exports = withPlugins([
+	[withBundleAnalyzer],
+	[withImages]
+], {
+	webpack(config, options) {
+		config.resolve.modules.push(path.resolve('./'));
+		return config;
+	},
+	async headers() {
+		return [
+			{ source: '/', headers: securityHeaders },
+			{ source: '/:path*', headers: securityHeaders },
+			{
+				// matching all API routes
+				source: '/api/:path*',
+				headers: [
+					{ key: 'Access-Control-Allow-Credentials', value: 'true' },
+					{ key: 'Access-Control-Allow-Origin', value: '*' },
+					{ key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,POST' },
+					// eslint-disable-next-line max-len
+					{ key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version' },
+				],
+			},
+		];
+	},
+});
---
+++ pages/_document.js
@@ -0,0 +1,95 @@
+import React from "react";
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <meta charSet="utf-8" />
+          <meta name="theme-color" content="#000000" />
+          <meta
+            property="og:description"
+            // eslint-disable-next-line max-len
+            content="Working clock in the style of It's Always Sunny in Philadelphia"
+          />
+          <meta
+            property="og:title"
+            // eslint-disable-next-line max-len
+            content="Always Sunny Clock"
+          />
+          <meta
+            name="description"
+            // eslint-disable-next-line max-len
+            content="Working clock in the style of It's Always Sunny in Philadelphia"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+          <meta name="msapplication-TileColor" content="#da532c" />
+          <meta name="theme-color" content="#ffffff" />
+        </Head>
+        <body>
+          <div id="page-transition"></div>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+MyDocument.getInitialProps = async (ctx) => {
+  // Resolution order
+  //
+  // On the server:
+  // 1. app.getInitialProps
+  // 2. page.getInitialProps
+  // 3. document.getInitialProps
+  // 4. app.render
+  // 5. page.render
+  // 6. document.render
+  //
+  // On the server with error:
+  // 1. document.getInitialProps
+  // 2. app.render
+  // 3. page.render
+  // 4. document.render
+  //
+  // On the client
+  // 1. app.getInitialProps
+  // 2. page.getInitialProps
+  // 3. app.render
+  // 4. page.render
+
+
+  const initialProps = await Document.getInitialProps(ctx);
+
+  return {
+    ...initialProps,
+    // Styles fragment is rendered after the app and page rendering finish.
+    styles: [
+      <React.Fragment key="styles">
+        {initialProps.styles}
+      </React.Fragment>,
+    ],
+  };
+};
+
+export default MyDocument;
